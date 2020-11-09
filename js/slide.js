6f462eaa63df4d7915faf4c93bd17ffd4283223e
desarrollowebtony <desarrollowebtony@gmail.com>
2020-11-08 23:21:00 -0600

    correccion camisas

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,6 @@
 let previousBtn = document.getElementById("previousBtn");
 let item = document.getElementById("slideImg");
-const slideArray = ['img/3.png', 'img/gorras/Gorras-07.png', 'img/1.png', 'img/gorras/Gorras-06.png', 'img/4.png', 'img/gorras/1.png', 'img/camisasMujeres/camisa11.png'];
+const slideArray = ['img/3.png', 'img/gorras/Gorras-07.png', 'img/1.png', 'img/camisasMujeres/camisas manga corta11.png', 'img/4.png', 'img/gorras/1.png', 'img/camisasMujeres/camisas manga corta8.png'];
 let index = 0;
 
 console.log(slideArray[0]);
